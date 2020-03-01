54cd09dc1158b69b994db0ffc2af158a554b017b
Jonathan Sick <jonathan@jsick.codes>
2020-02-29 23:20:34 -0500

    Add highlighting to result card

--- src/templates.js
+++ src/templates.js
@@ -1,13 +1,19 @@
 // Templates for search result items and listings
 
+// Empty results template
+// https://www.algolia.com/doc/api-reference/widgets/hits/js/#widget-param-empty
 export const emptyTemplate = `
 <p>No results for <q>{{ query }}.</q></p>
 `;
 
+// Result item template
+// https://www.algolia.com/doc/api-reference/widgets/hits/js/#widget-param-item
+// important: any attribute used with helpers.highlight *must* be configured
+// in the index as "Attributes to highlight"
 export const itemTemplate = `
 <article class="search-item">
-  <h2>{{ h1 }}</h2>
+  <h2>{{#helpers.highlight}}{ "attribute": "h1" }{{/helpers.highlight}}</h2>
   <img src={{ thumbnail }} alt="">
-  <p>{{ content }}</p>
+  <p>{{#helpers.highlight}}{ "attribute": "content" }{{/helpers.highlight}}</p>
 </article>
 `;
