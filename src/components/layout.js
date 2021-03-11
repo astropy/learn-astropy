d7ae91f2b796b5efeeb5c9e6294d0e8a1119bf6b
Jonathan Sick <jonathan@jsick.codes>
2021-03-11 10:30:56 -0500

    Add a container for content in the layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import GlobalStyles from '../styles/globalStyles';
 import Header from './header';
+
+const StyledMain = styled.main`
+  margin: 0 auto;
+  max-width: 60em;
+  padding: 0 10px 0 10px;
+`;
 
 /*
  * The Layout component wraps the contents of every page.
@@ -12,7 +19,7 @@ export default function Layout({ children }) {
     <>
       <GlobalStyles />
       <Header />
-      {children}
+      <StyledMain>{children}</StyledMain>
     </>
   );
 }
