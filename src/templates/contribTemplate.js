7dddfd7680946d7851358c0ea068c270da46b7fb
Jonathan Sick <jonathan@jsick.codes>
2021-03-31 11:22:31 -0400

    Use Layout component with contribTemplate

--- src/templates/contribTemplate.js
+++ src/templates/contribTemplate.js
@@ -7,6 +7,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
 export const pageQuery = graphql`
   query($slug: String!) {
     markdownRemark(frontmatter: { slug: { eq: $slug } }) {
@@ -19,20 +22,25 @@ export const pageQuery = graphql`
   }
 `;
 
-export default function Template({ data }) {
+export default function Template({ data, location }) {
   const { markdownRemark } = data; // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark;
 
   return (
-    <div className="doc-container">
+    <Layout>
+      <SEO location={location} title="Home" />
+
       <h1>{frontmatter.title}</h1>
       {/* eslint-disable react/no-danger */}
       <div className="doc-content" dangerouslySetInnerHTML={{ __html: html }} />
       {/* eslint-enable react/no-danger */}
-    </div>
+    </Layout>
   );
 }
 
 Template.propTypes = {
   data: PropTypes.object.isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }),
 };
