463ac09365da043fa59fac88158aca9f1609a006
Jonathan Sick <jonathan@jsick.codes>
2021-04-15 12:43:40 -0400

    Add the InstantSearch component to /tutorials

--- src/pages/tutorials.js
+++ src/pages/tutorials.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import searchClient from '../searchClient';
 
 export default function TutorialsPage({ location }) {
   return (
@@ -14,6 +16,14 @@ export default function TutorialsPage({ location }) {
         incorporate several packages. They are more specific and less conceptual
         than Guides but more extended than Examples.
       </p>
+
+      <InstantSearch
+        searchClient={searchClient}
+        indexName="astropy_fulltext_dev"
+      >
+        <SearchBox />
+        <Hits />
+      </InstantSearch>
     </Layout>
   );
 }
