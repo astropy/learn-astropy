1f4a4894d27d98b1eb075a060f1ea288e83d78c8
adrn <adrn@users.noreply.github.com>
2021-09-28 15:29:58 +0000

    deploy: ca5e78f15428e9652a524113b7454c78d88e45d5

---
+++ component---src-templates-contrib-template-js-261fa6b9b76685648f29.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklearn_astropy=self.webpackChunklearn_astropy||[]).push([[868],{2866:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var a=n(7294),r=n(6474),l=n(3751);function c(e){var t=e.data,n=e.location,c=t.markdownRemark,o=c.frontmatter,u=c.html;return a.createElement(r.Z,null,a.createElement(l.Z,{location:n,title:"Home"}),a.createElement("h1",null,o.title),a.createElement("div",{className:"doc-content",dangerouslySetInnerHTML:{__html:u}}))}}}]);
+//# sourceMappingURL=component---src-templates-contrib-template-js-261fa6b9b76685648f29.js.map
