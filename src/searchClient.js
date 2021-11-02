b88cac0080877f26385bacddd6d8a1ef7221f461
Jonathan Sick <jonathan@jsick.codes>
2021-11-01 20:53:26 -0400

    Switch to "prod" Algolia app
    The new Algolia App is called "Learn Astropy" and the main index is
    called "prod_learn_astropy". The replica index for the priority-based
    sort is "prod_learn_astropy_priority".

--- src/searchClient.js
+++ src/searchClient.js
@@ -2,8 +2,8 @@ import algoliasearch from 'algoliasearch/lite';
 
 // This is the Search-only API key
 const searchClient = algoliasearch(
-  'KSDFJKHCO2',
-  'fadf7c74af324735d9e45d68481531ab'
+  'H6MWLDHTG5',
+  '45fc58ab091520ba879eb8952b422c4e'
 );
 
 export default searchClient;
