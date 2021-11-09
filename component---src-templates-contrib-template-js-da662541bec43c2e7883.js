5b79814d5d18ea3127436d1899c8b7eaf2fb659d
adrn <adrn@users.noreply.github.com>
2021-11-09 16:07:59 +0000

    deploy: d4b6b63733871835d8e98687cf2d7b48f7b00cf4

---
+++ component---src-templates-contrib-template-js-da662541bec43c2e7883.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklearn_astropy=self.webpackChunklearn_astropy||[]).push([[868],{2866:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var a=n(7294),r=n(922),l=n(3751);function c(e){var t=e.data,n=e.location,c=t.markdownRemark,o=c.frontmatter,u=c.html;return a.createElement(r.Z,null,a.createElement(l.Z,{location:n,title:"Home"}),a.createElement("h1",null,o.title),a.createElement("div",{className:"doc-content",dangerouslySetInnerHTML:{__html:u}}))}}}]);
+//# sourceMappingURL=component---src-templates-contrib-template-js-da662541bec43c2e7883.js.map
